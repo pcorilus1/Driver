a14f6a126a6f2f44422768b2e0567cb148ec9eb0
pcorilus1 <peter1.corilus@famu.edu>
2020-03-12 05:01:56 -0400

    Commit with Nav

--- app/src/main/java/net/androidbootcamp/myapplication/Home.java
+++ app/src/main/java/net/androidbootcamp/myapplication/Home.java
@@ -2,12 +2,28 @@ package net.androidbootcamp.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class Home extends AppCompatActivity {
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
 
     Button btn_Jobs;
     Button btn_Account;
@@ -17,6 +33,17 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigationView);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         btn_Jobs = findViewById(R.id.btn_Jobs);
         btn_Jobs.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +58,7 @@ public class Home extends AppCompatActivity {
 
         btn_Account = findViewById(R.id.btn_Account);
         btn_Account.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent inToMain = new Intent(Home.this, Vendor_Items.class);
@@ -55,5 +83,29 @@ public class Home extends AppCompatActivity {
 
 
 
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.profile:
+                Toast.makeText(Home.this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.contact:
+                Toast.makeText(Home.this, "Contact us Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about:
+                Toast.makeText(Home.this, "About us Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(Home.this, "Logout Selected", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 }
